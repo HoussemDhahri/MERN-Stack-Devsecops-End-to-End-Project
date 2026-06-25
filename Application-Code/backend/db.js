2163db595717d06b1a5820052249b72f148a0798
HoussemDhahri <dhahri_houssem@hotmail.com>
2026-06-25 18:22:37 +0100

    add project

---
+++ Application-Code/backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        const useDBAuth = process.env.USE_DB_AUTH || false;
+
+        const connectionParams = {};
+
+        if (useDBAuth) {
+            connectionParams.user = process.env.MONGO_USERNAME;
+            connectionParams.pass = process.env.MONGO_PASSWORD;
+        }
+
+        await mongoose.connect(process.env.MONGO_CONN_STR, connectionParams);
+
+        console.log("Connected to database.");
+    } catch (error) {
+        console.log("Could not connect to database.", error);
+    }
+};
